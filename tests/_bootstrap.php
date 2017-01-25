fdc35ffe9f68e7fad7add8c89f09d78dd5e1b089
Andrii Vasyliev <sol@hiqdev.com>
2017-01-25 09:36:53 +0000

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Guzzle transport for yii2-hiart
+ *
+ * @link      https://github.com/hiqdev/yii2-hiart-guzzle
+ * @package   yii2-hiart-guzzle
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(-1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+use yii\console\Application;
+use hiqdev\composer\config\Builder;
+
+Yii::$app = new Application(array_merge([
+    'id' => 'yii2-hiart-guzzle-tests',
+    'basePath' => __DIR__,
+], require Builder::path('hidev')));
