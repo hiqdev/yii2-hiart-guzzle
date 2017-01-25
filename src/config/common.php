a15f17a8a15f10e3d37835c0de61664d72bb8c87
Andrii Vasyliev <sol@hiqdev.com>
2017-01-25 10:01:50 +0000

    fixed config

--- src/config/common.php
+++ src/config/common.php
@@ -8,30 +8,4 @@
  * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
  */
 
-return empty($params['hiart.enabled']) ? [] : [
-    'modules' => array_filter([
-        'debug' => empty($params['debug.enabled']) ? null : [
-            'panels' => [
-                'hiart' => [
-                    'class' => \hiqdev\hiart\debug\DebugPanel::class,
-                ],
-            ],
-        ],
-    ]),
-    'components' => array_filter([
-        $params['hiart.dbname'] => array_filter([
-            'class' => $params['hiart.class'],
-            'requestClass' => $params['hiart.requestClass'],
-            'name' => $params['hiart.dbname'],
-            'config' => $params['hiart.config'],
-            'baseUri' => $params['hiart.baseUri'],
-        ]),
-    ]),
-    'container' => [
-        'singletons' => [
-            \hiqdev\hiart\ConnectionInterface::class => function () {
-                return Yii::$app->get(Yii::$app->params['hiart.dbname']);
-            },
-        ],
-    ],
-];
+return [];
--- src/config/params.php
+++ src/config/params.php
@@ -9,10 +9,5 @@
  */
 
 return [
-    'hiart.enabled'         => true,
-    'hiart.class'           => \hiqdev\hiart\rest\Connection::class,
-    'hiart.requestClass'    => \hiqdev\hiart\stream\Request::class,
-    'hiart.dbname'          => 'hiart',
-    'hiart.config'          => [],
-    'hiart.baseUri'         => null,
+    'hiart.requestClass'    => \hiqdev\hiart\guzzle\Request::class,
 ];
